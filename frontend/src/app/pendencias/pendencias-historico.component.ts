a557785742697642e513eb8233cbac79f5a84eb9
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-14 19:56:00 -0300

    fix: remover import nao usado de inject no progresso

--- frontend/src/app/pendencias/pendencias-historico.component.ts
+++ frontend/src/app/pendencias/pendencias-historico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import {
