ae49a161a4df9a8c893fe802fd3294db28c0a51e
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: frontend/src/app/solicitacao/submissao/submissao-solicitacao.component.ts

--- frontend/src/app/solicitacao/submissao/submissao-solicitacao.component.ts
+++ frontend/src/app/solicitacao/submissao/submissao-solicitacao.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, signal } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { SolicitacaoDetalhe } from '../solicitacao.model';
@@ -11,7 +11,7 @@ import { rotuloStatus } from '../status-solicitacao';
   imports: [CommonModule, RouterLink],
   templateUrl: './submissao-solicitacao.component.html',
 })
-export class SubmissaoSolicitacaoComponent {
+export class SubmissaoSolicitacaoComponent implements OnInit {
   private readonly solicitacaoService = inject(SolicitacaoService);
 
   @Input() atividadeId?: number;
