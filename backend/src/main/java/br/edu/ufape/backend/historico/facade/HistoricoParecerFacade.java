f7e8dc057f3bfacb90c5229cee0b49d5d7ea13a1
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: backend/src/main/java/br/edu/ufape/backend/historico/facade/HistoricoParecerFacade.java

--- backend/src/main/java/br/edu/ufape/backend/historico/facade/HistoricoParecerFacade.java
+++ backend/src/main/java/br/edu/ufape/backend/historico/facade/HistoricoParecerFacade.java
@@ -11,17 +11,17 @@ import br.edu.ufape.backend.historico.service.HistoricoParecerService;
 @Component
 public class HistoricoParecerFacade {
 
-    private final HistoricoParecerService service;
+	private final HistoricoParecerService service;
 
-    public HistoricoParecerFacade(HistoricoParecerService service) {
-        this.service = service;
-    }
+	public HistoricoParecerFacade(HistoricoParecerService service) {
+		this.service = service;
+	}
 
-    public List<AtividadeComHistoricoParecerDTO> listarPorEstudante() {
-        return service.listarPorEstudante();
-    }
+	public List<AtividadeComHistoricoParecerDTO> listarPorEstudante() {
+		return service.listarPorEstudante();
+	}
 
-    public List<ParecerAvaliadorHistoricoDTO> buscarPorAtividade(Long id) {
-        return service.buscarPorAtividade(id);
-    }
+	public List<ParecerAvaliadorHistoricoDTO> buscarPorAtividade(Long id) {
+		return service.buscarPorAtividade(id);
+	}
 }
