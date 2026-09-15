6825e5415f0f1e6b3d9b83b1288e379b55a24968
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: backend/src/main/java/br/edu/ufape/backend/solicitacao/model/ParecerPorAtividade.java

--- backend/src/main/java/br/edu/ufape/backend/solicitacao/model/ParecerPorAtividade.java
+++ backend/src/main/java/br/edu/ufape/backend/solicitacao/model/ParecerPorAtividade.java
@@ -1,12 +1,5 @@
 package br.edu.ufape.backend.solicitacao.model;
 
-public record ParecerPorAtividade(
-    Long atividadeId,
-    String status,
-    String justificativa,
-    String tipoParecer,
-    String observacoes,
-    String artigoRegulamento,
-    Integer cargaHorariaAproveitavel,
-    String dataAvaliacao
-) {}
+public record ParecerPorAtividade(Long atividadeId, String status, String justificativa, String tipoParecer,
+		String observacoes, String artigoRegulamento, Integer cargaHorariaAproveitavel, String dataAvaliacao) {
+}
