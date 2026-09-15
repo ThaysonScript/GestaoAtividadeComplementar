46ef39086ece4033e497fe26b4ca21817e96dc27
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: backend/src/main/java/br/edu/ufape/backend/historico/dto/ParecerAvaliadorHistoricoDTO.java

--- backend/src/main/java/br/edu/ufape/backend/historico/dto/ParecerAvaliadorHistoricoDTO.java
+++ backend/src/main/java/br/edu/ufape/backend/historico/dto/ParecerAvaliadorHistoricoDTO.java
@@ -1,13 +1,6 @@
 package br.edu.ufape.backend.historico.dto;
 
-public record ParecerAvaliadorHistoricoDTO(
-    Long id,
-    Long atividadeId,
-    String dataAvaliacao,
-    String statusSolicitacao,
-    String tipoParecer,
-    String justificativa,
-    String observacoes,
-    String artigoRegulamento,
-    Integer cargaHorariaAproveitavel
-) {}
+public record ParecerAvaliadorHistoricoDTO(Long id, Long atividadeId, String dataAvaliacao, String statusSolicitacao,
+		String tipoParecer, String justificativa, String observacoes, String artigoRegulamento,
+		Integer cargaHorariaAproveitavel) {
+}
