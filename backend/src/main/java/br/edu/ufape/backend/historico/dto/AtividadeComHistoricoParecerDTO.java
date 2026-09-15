35b1323e5207e8336960f6fe417c9d4121e02d50
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: backend/src/main/java/br/edu/ufape/backend/historico/dto/AtividadeComHistoricoParecerDTO.java

--- backend/src/main/java/br/edu/ufape/backend/historico/dto/AtividadeComHistoricoParecerDTO.java
+++ backend/src/main/java/br/edu/ufape/backend/historico/dto/AtividadeComHistoricoParecerDTO.java
@@ -2,13 +2,7 @@ package br.edu.ufape.backend.historico.dto;
 
 import java.util.List;
 
-public record AtividadeComHistoricoParecerDTO(
-    Long atividadeId,
-    String titulo,
-    String natureza,
-    String categoria,
-    Integer cargaHorariaEmHoras,
-    String statusAtual,
-    Boolean pendenciasAtivas,
-    List<ParecerAvaliadorHistoricoDTO> pareceres
-) {}
+public record AtividadeComHistoricoParecerDTO(Long atividadeId, String titulo, String natureza, String categoria,
+		Integer cargaHorariaEmHoras, String statusAtual, Boolean pendenciasAtivas,
+		List<ParecerAvaliadorHistoricoDTO> pareceres) {
+}
