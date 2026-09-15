996988871fe896c425fca4d0de6244ae9a335466
ThaysonScript <thaysonguedes01@gmail.com>
2026-09-15 15:31:21 -0300

    Modificado: backend/src/main/java/br/edu/ufape/backend/solicitacao/dto/SolicitacaoAtividadeResponseDTO.java

--- backend/src/main/java/br/edu/ufape/backend/solicitacao/dto/SolicitacaoAtividadeResponseDTO.java
+++ backend/src/main/java/br/edu/ufape/backend/solicitacao/dto/SolicitacaoAtividadeResponseDTO.java
@@ -2,10 +2,10 @@ package br.edu.ufape.backend.solicitacao.dto;
 
 import br.edu.ufape.backend.solicitacao.model.SolicitacaoAtividade;
 
-public record SolicitacaoAtividadeResponseDTO(Long atividadeId, String titulo, Integer cargaHoraria,
-		String natureza, String status, String justificativa) {
+public record SolicitacaoAtividadeResponseDTO(Long atividadeId, String titulo, Integer cargaHoraria, String natureza,
+		String status, String justificativa) {
 	public SolicitacaoAtividadeResponseDTO(SolicitacaoAtividade item) {
-		this(item.getAtividadeId(), item.getTitulo(), item.getCargaHoraria(), item.getNatureza(),
-				item.getStatus(), item.getJustificativa());
+		this(item.getAtividadeId(), item.getTitulo(), item.getCargaHoraria(), item.getNatureza(), item.getStatus(),
+				item.getJustificativa());
 	}
 }
